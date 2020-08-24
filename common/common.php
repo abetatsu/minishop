f8deaf425846855945ca13ef766254f5392d8824
Tatsu <tatsuhiroabe0127@gmail.com>
2020-08-25 08:29:52 +0900

    first commit

---
+++ common/common.php
@@ -0,0 +1,18 @@
+<?php
+function sanitize($post)
+{
+     foreach ($post as $key => $value) {
+          $sanitized_post[$key] = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+     }
+     return $sanitized_post;
+}
+
+function connectDB()
+{
+     $dsn = 'mysql:dbname=shop; host=localhost; charset=utf8';
+     $user = 'root';
+     $password = 'root';
+     $dbh = new PDO($dsn, $user, $password);
+     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     return $dbh;
+}
---
+++ product/pro_ng.php
@@ -0,0 +1,7 @@
+<?php
+
+echo <<<EOD
+  商品が選択されていません<br>
+  <a href="pro_list.php">戻る</a>
+EOD;
+?>
